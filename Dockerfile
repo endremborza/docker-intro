872657ad83649b1029dab622b47946a6df26a14d
Endre Márk Borza <endremborza@gmail.com>
2020-03-29 17:46:51 +0200

    v3

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,4 @@ COPY ./requirements.txt /requirements.txt
 
 RUN pip install -r /requirements.txt
 
-COPY ./script.py /script.py
-
-CMD ["python", "/script.py"]
+CMD ["bash"]
--- app/script.py
+++ app/script.py
@@ -6,4 +6,4 @@ def hello():
     return '<h2>Hello from the container!</h2>'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5432)
+    app.run(host="0.0.0.0", port=5432, debug=True)
